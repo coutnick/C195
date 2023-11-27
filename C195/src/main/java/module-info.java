345730f65631627419ecc631dbaa52263c1c82af
coutnick <nrains1020@gmail.com>
2023-11-28 07:14:41 +1400

    added error check for if the appointment list was null when trying to delete Removed required statement added dependency

--- C195/src/main/java/module-info.java
+++ C195/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.c195 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.j;
+
 
 
     opens com.example.c195 to javafx.fxml;
